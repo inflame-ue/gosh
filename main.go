4c5d199ff172d96477af478454a2367a805dc3e0
Ilia Oberemok <i.oberemok777@gmail.com>
2026-05-28 19:11:30 +0300

    feat: gracefully handle ctrl-c plus extract the scanner into its own goroutine

--- main.go
+++ main.go
@@ -13,62 +13,57 @@ import (
 )
 
 func repl() {
-	scanner := bufio.NewScanner(os.Stdin)
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+
+	input := make(chan string)
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			input <- scanner.Text()
+		}
+	}()
+
 	for {
 		fmt.Print("gosh> ")
-		scanner.Scan()
-
-		if err := scanner.Err(); err != nil {
-			fmt.Printf("err: %v\n", err)
-			continue
-		}
-
-		input := scanner.Text()
-		if len(input) == 0 {
+		select {
+		case <-sigc:
 			fmt.Println()
 			continue
-		}
-
-		commands, err := parser.ParseCommands(input)
-		if err != nil {
-			fmt.Printf("err: %v\n", err)
-			continue
-		}
-
-		if len(commands) > 1 {
-			err := command.ExecuteCommands(commands)
+		case line := <-input:
+			commands, err := parser.ParseCommands(line)
 			if err != nil {
 				fmt.Printf("err: %v\n", err)
+				continue
 			}
 
-			// TODO: the builtins do not support pipes?
-			// i guess we skip to $PATH programs here anyway, so builtins are just for fancy demonstration purposes
-			// that i am capable of implementing a subset of shell commands (why?)
-			continue
-		}
+			if len(commands) > 1 {
+				err := command.ExecuteCommands(commands)
+				if err != nil {
+					fmt.Printf("err: %v\n", err)
+				}
 
-		command := commands[0]
-		if handler, ok := builtins.BuiltinCommands[command.Name]; ok {
-			handler(*command)
-			continue
-		}
+				// TODO: the builtins do not support pipes?
+				// i guess we skip to $PATH programs here anyway, so builtins are just for fancy demonstration purposes
+				// that i am capable of implementing a subset of shell commands (why?)
+				continue
+			}
 
-		err = command.Execute()
-		if err != nil {
-			fmt.Printf("err: %v\n", err)
-			continue
+			command := commands[0]
+			if handler, ok := builtins.BuiltinCommands[command.Name]; ok {
+				handler(*command)
+				continue
+			}
+
+			err = command.Execute()
+			if err != nil {
+				fmt.Printf("err: %v\n", err)
+				continue
+			}
 		}
 	}
-
 }
 
 func main() {
-	sigc := make(chan os.Signal, 1)
-	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		<-sigc
-		fmt.Println("\ninterrupt received...exiting...")
-		os.Exit(1)
-	}()
 	repl()
 }
